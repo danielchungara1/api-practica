eedd1f15dbbc13003c168f90572439a127af1b90
daniel <danielchungaar1@gmail.com>
2020-05-23 03:15:11 -0300

    Soporte para metamodelos.

--- src/main/java/com/api/practica/meli/ProductoMeliBusiness.java
+++ src/main/java/com/api/practica/meli/ProductoMeliBusiness.java
@@ -1,6 +1,7 @@
 package com.api.practica.meli;
 
 import com.api.practica.dtos.CollectionPaginatedDto;
+import com.api.practica.productos.Producto_;
 import com.api.practica.services.BackendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,25 +15,27 @@ public class ProductoMeliBusiness {
 	BackendService backendService;
 
 	public CollectionPaginatedDto<ProductoMeliDto> getProductosPorNombre(String nombre, Integer limite, Integer offset) throws Exception {
+
 		return this.sendGet(nombre, limite, offset);
 	}
 
 	private CollectionPaginatedDto<ProductoMeliDto> sendGet(String txt, Integer limite, Integer offset) throws Exception {
 
-		//Busco los productos
-		txt = txt.replace(" ", "%20");
-		String endPointProductos = String.format("https://api.mercadolibre.com/sites/MLA/search?q=%s&offset=%d&limit=%d", txt, offset, limite);
-
-		CollectionPaginatedDto<ProductoMeliDto> productosPaginados =
-				this.backendService.getCollectionPaginatedDto(endPointProductos, ProductoMeliDto.class);
-
-		//Agrego las imagenes
-		for (ProductoMeliDto productoMeliDto : productosPaginados.getResults()) {
-			String endPointItem = "https://api.mercadolibre.com/items/" + productoMeliDto.getId();
-			List<PictureItemDto> pictures = this.backendService.getArray(endPointItem, "pictures", PictureItemDto[].class);
-			productoMeliDto.setPictures(pictures);
-		}
-
-		return productosPaginados;
+//		//Busco los productos
+//		txt = txt.replace(" ", "%20");
+//		String endPointProductos = String.format("https://api.mercadolibre.com/sites/MLA/search?q=%s&offset=%d&limit=%d", txt, offset, limite);
+//
+//		CollectionPaginatedDto<ProductoMeliDto> productosPaginados =
+//				this.backendService.getCollectionPaginatedDto(endPointProductos, ProductoMeliDto.class);
+//
+//		//Agrego las imagenes
+//		for (ProductoMeliDto productoMeliDto : productosPaginados.getResults()) {
+//			String endPointItem = "https://api.mercadolibre.com/items/" + productoMeliDto.getId();
+//			List<PictureItemDto> pictures = this.backendService.getArray(endPointItem, "pictures", PictureItemDto[].class);
+//			productoMeliDto.setPictures(pictures);
+//		}
+//
+//		return productosPaginados;
+		return null;
 	}
 }
