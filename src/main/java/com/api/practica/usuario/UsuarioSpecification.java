3c80fb96f3184f07f61a6745023fbd3ef434a788
daniel <danielchungaar1@gmail.com>
2020-05-24 04:28:37 -0300

    Parcial.

--- src/main/java/com/api/practica/usuario/UsuarioSpecification.java
+++ src/main/java/com/api/practica/usuario/UsuarioSpecification.java
@@ -4,7 +4,9 @@ import com.api.practica.commons.entity.BaseEntity_;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 public class UsuarioSpecification {
     public static Specification<Usuario> getUsersByEmailSpec(String email) {
@@ -16,7 +18,7 @@ public class UsuarioSpecification {
     public static Specification<Usuario> getUsersByAll(String textSearch) {
         return (root, query, criteriaBuilder) -> {
             //id
-            Predicate criteriaId = criteriaBuilder.equal(root.get(Usuario_.id),textSearch.matches("[0-9]+")? Long.valueOf(textSearch) :null);
+            Predicate criteriaId = criteriaBuilder.equal(root.get("id"), textSearch.matches("[1-9]+")?Long.valueOf(textSearch):null);
 
             //email
             Predicate criteriaEmail = criteriaBuilder.like(root.get(Usuario_.email), "%"+textSearch+"%");
